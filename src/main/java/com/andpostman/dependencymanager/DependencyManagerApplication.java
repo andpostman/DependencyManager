b6501db1e8ee6ef0eee6e70089f28ecfd8cd7206
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-26 13:42:32 +0300

    Add application runner

---
+++ src/main/java/com/andpostman/dependencymanager/DependencyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.dependencymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyManagerApplication.class, args);
+    }
+
+}
