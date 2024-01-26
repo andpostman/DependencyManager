1436c2ab0f6fbe79eb19691a8658b743c6347522
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-26 13:46:08 +0300

    Add kafka config

---
+++ src/main/java/com/andpostman/dependencymanager/config/KafkaProducerConfig.java
@@ -0,0 +1,46 @@
+package com.andpostman.dependencymanager.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerConfig {
+
+    @Value("${topic.name}")
+    private String eventTopic;
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        // get configs on application.yml
+        Map<String, Object> properties = kafkaProperties.buildProducerProperties();
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder
+                .name(eventTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+
+}
