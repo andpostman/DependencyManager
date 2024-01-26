f2f277cdb74ff6e93ec743fe16148a0587184937
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-26 13:43:47 +0300

    Add default test

---
+++ src/test/java/com/andpostman/dependencymanager/DependencyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.dependencymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
